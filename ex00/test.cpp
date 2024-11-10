15234611fd31cc3ea9437ea58f866a203dc77c03
Goodman <=>
2024-11-10 14:34:35 +0100

    Done

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -9,10 +9,16 @@ namespace myNameSpace
     {
         std::cout << "Hello from Me" << std::endl;
     }
+    void doSomething()
+    {
+	    std::cout << "i'm just student now i cant do sothing just study" << std::endl;
+    }
+
 } // namespalsce myNameSpace
 
 int main(int argc, char **argv)
 {
     std::cout << "Hello from Main function" << std::endl;
-    myNameSpace::sayHey;
+    myNameSpace::sayHey();
+    myNameSpace::doSomething();
 }
