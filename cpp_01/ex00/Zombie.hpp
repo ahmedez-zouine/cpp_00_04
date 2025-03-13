77bee7e1a6de62775a4a04870f80d17be3cf64be
ahmedez-zouine <zouineahmed976@gmail.com>
2025-03-13 19:40:47 +0000

    Done

--- cpp_01/ex00/Zombie.hpp
+++ cpp_01/ex00/Zombie.hpp
@@ -1,5 +1,5 @@
-#ifndef ZOMBIE_CPP
-#define ZOMBIE_CPP
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
 
 #include <iostream>
 
---
+++ cpp_01/ex05/l/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main()
+{
+    Harl karen;
+
+    karen.complain("INFO");
+    karen.complain("ERROR");
+    karen.complain("herl");
+    karen.complain("DEBUG");
+    karen.complain("WARNING");
+}
